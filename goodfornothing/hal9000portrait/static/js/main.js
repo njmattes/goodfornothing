3690ebee94ed72fbf70cbf156efd9cc237a52f81
Nathan Matteson <matteson@obstructures.org>
2020-01-16 17:19:52 -0600

    Update JavaScript variables

--- goodfornothing/hal9000portrait/static/js/main.js
+++ goodfornothing/hal9000portrait/static/js/main.js
@@ -1,26 +1,24 @@
-
 (function() {
   'use strict';
 
-  var body = d3.select('div')
-    , timer
-    , i = 0
-    , time = 0
-  ;
+  const hal_eye = d3.select('div');
+  const CSV_URL = '/hal9000portrait/static/csv/hal_rows.csv';
+  let timer;
+  let i = 0;
+  let time = 0;
 
-  var change_color = function change_color(color) {
-    body.style('background-color', color.color);
+  const change_color = function change_color(color) {
+    hal_eye.style('background-color', color.color);
   };
 
-  d3.csv('/hal9000portrait/static/csv/hal_rows.csv', function(error, data) {
+  d3.csv(CSV_URL, function(error, data) {
 
-    var colors = data
-      , n = colors.length
-    ;
+    const COLORS = data;
+    const N = COLORS.length;
 
     function repeat() {
-      change_color(colors[i]);
-      i = i < n - 1 ? i + 1 : 0;
+      change_color(COLORS[i]);
+      i = i < N - 1 ? i + 1 : 0;
       timer = setTimeout(
         repeat,
         time
