0b985435eab1ad72d233205e532f52bfa76c7c72
Nathan Matteson <matteson@obstructures.org>
2020-01-17 12:12:26 -0600

    Update to d3.js v5 and make structure of code less stupid.

--- goodfornothing/hal9000portrait/static/js/main.js
+++ goodfornothing/hal9000portrait/static/js/main.js
@@ -4,29 +4,29 @@
   const hal_eye = d3.select('div');
   const CSV_URL = '/hal9000portrait/static/csv/hal_rows.csv';
   let timer;
-  let i = 0;
   let time = 0;
+  let n;
 
-  const change_color = function change_color(color) {
-    hal_eye.style('background-color', color.color);
+  const change_color = function change_color(row) {
+    hal_eye.style('background-color', row.color);
   };
 
-  d3.csv(CSV_URL, function(error, data) {
-
-    const COLORS = data;
-    const N = COLORS.length;
-
-    function repeat() {
-      change_color(COLORS[i]);
-      i = i < N - 1 ? i + 1 : 0;
+  const repeat = function repeat(json, i) {
+      change_color(json[i]);
       timer = setTimeout(
         repeat,
-        time
+        time,
+        json,
+        (i + 1) % n
       );
-    }
+    };
 
-    repeat();
-
-  });
+  d3.csv(CSV_URL, {
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }}).then(json => {
+      n = json.length;
+      repeat(json, 0);
+    });
 
 })();
--- goodfornothing/selfie1/static/js/main.js
+++ goodfornothing/selfie1/static/js/main.js
@@ -6,45 +6,59 @@
   const HEIGHT = 300;
   const PIXEL = 2;
 
-  d3.json(JSON_URL, function(error, data) {
+  let time;
+  let timer;
+  let n;
 
-    const canvas = d3.select('main')
-      .append('canvas')
-      .attr('width', WIDTH * PIXEL)
-      .attr('height', HEIGHT * PIXEL);
-    const context = canvas.node().getContext('2d');
-    let time = data.idx[1] / 30;
-    let timer;
-    let i = 0;
-    let pixels = [];
-    let max_idx_a = d3.max(d3.entries(data.ab), function(d) {
-        return d['value'].length > 0 ? d['key'] : 0 });
+  const context = d3.select('main')
+    .append('canvas')
+    .attr('width', WIDTH * PIXEL)
+    .attr('height', HEIGHT * PIXEL)
+    .node()
+    .getContext('2d');
 
-    const draw_pixels = function draw_pixels() {
-      pixels = data.a[data.idx[i]].concat(data.b[data.idx[i]]);
-      for (let p = 0; p < pixels.length; ++p) {
-        context.beginPath();
-        context.rect(pixels[p][1] * PIXEL, pixels[p][0] * PIXEL,
-          PIXEL, PIXEL);
-        context.fillStyle='rgb('+pixels[p][2].join(',')+')';
-        context.fill();
-        context.closePath();
-      }
-    };
+  const draw_pixel = function draw_pixel(x, y, rgb) {
+    context.beginPath();
+    context.rect(x * PIXEL, y * PIXEL, PIXEL, PIXEL);
+    context.fillStyle='rgb('+rgb.join(',')+')';
+    context.fill();
+    context.closePath();
+  };
 
-    const repeat = function repeat() {
-      draw_pixels(pixels);
-      i++;
-      if (i >= data.idx.length) {
-        i = 0;
-      }
-      timer = setTimeout(
-        repeat,
-        time
-      );
-    };
+  const draw_pixels = function draw_pixels(json, i) {
 
-    repeat();
+    let pixels = json.a[json.idx[i]].concat(json.b[json.idx[i]]);
+
+    for (let p = 0; p < pixels.length; ++p) {
+      let pixel = pixels[p];
+      draw_pixel(pixel[1], pixel[0], pixel[2]);
+    }
+
+  };
+
+  const repeat = function repeat(json, i) {
+
+    draw_pixels(json, i);
+
+    timer = setTimeout(
+      repeat,
+      time,
+      json,
+      (i + 1) % n
+    );
+
+  };
+
+  d3.json(JSON_URL, {
+    headers: {
+
+      'Content-type': 'application/json; charset=UTF-8'
+
+    }}).then(json => {
+
+      n = json.idx.length;
+      time = json.idx[1] / 30;
+      repeat(json, 0);
 
   });
 
