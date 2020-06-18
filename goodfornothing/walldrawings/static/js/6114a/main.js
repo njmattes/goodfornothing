0cbc1756dad209553e9920decf29fbccf21a44cb
Nathan Matteson <matteson@obstructures.org>
2020-06-18 04:09:25 -0500

    Fix images in 6114a that don't change

--- goodfornothing/walldrawings/static/js/6114a/main.js
+++ goodfornothing/walldrawings/static/js/6114a/main.js
@@ -7,23 +7,32 @@
 
   let n;
 
-  const wall = d3.select('main')
-    .append('img')
-    .attr('class', 'wall');
+  const wall = d3.select('main');
 
   const replace_image = function replace_image(walls, i) {
-    console.log(i);
-    wall.attr('src', WALL_DIR + walls[i % n]);
+    d3.selectAll('.wall').style('display', 'none');
+    d3.select(`.wall-${i}`).style('display', 'block');
     i = i > n ? 0 : i + 1;
     setTimeout(replace_image.bind({}, walls, i), TIMER);
   };
+
+  const add_images = function add_images(walls, n) {
+    for (let i = 0; i < n; ++i) {
+      wall
+        .append('img')
+        .attr('class', `wall wall-${i}`)
+        .attr('src', WALL_DIR + walls[i % n])
+        .style('display', 'none');
+    }
+  }
 
   const init = async function init() {
     let json = await d3.json(JSON_PATH,
       {headers: {'Content-type': 'application/json; charset=UTF-8'}});
     if (json.walls) {
       n = json.walls.length;
-      replace_image(json.walls, 0)
+      add_images(json.walls, n);
+      replace_image(json.walls, 0);
     }
   };
 
